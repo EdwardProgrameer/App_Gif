f113a99eff0ec08bb467bfdd58f890e5709a24a5
edwardProgrameer <edwardmanuelarredondo@gmail.com>
2022-10-05 00:38:09 -0400

    segundo commit de mi aplicacion

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,29 +1,40 @@
-import{useState} from 'react';
+import { useState } from "react";
+import { AddCategory } from "./Components/AddCategory";
+import {GifGrid} from './Components/GifGrid';
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
-const [categories, setCategories] = useState(['One Punch','Dragon Ball'])
+  const onAddCategory = (newCategory) => {
 
-const  onAddCategory = () => {
+    if (categories.includes(newCategory)) return
 
-// setCategories([...categories,'valorant'])
-setCategories ( cat => [...cat, 'valorant'])
-}
+    setCategories([newCategory, ...categories]);
+  };
 
   return (
     <>
-    {/*titulo */}
-    <h1>GifExpertApp</h1>
-    {/*Input */}
-    {/* listado de git */}
-    <button onClick={onAddCategory}>Agregar</button>
-    <ol>
-      {categories.map(category => {
-       return <li key = {category}>{category}</li>
-      })
-      }
-    </ol>
-    {/*git Item */}
+     
+      <h1>GifExpertApp</h1>
+
+      
+      <AddCategory
+        onNewCategory={(value) => onAddCategory(value)}
+      />
+
+      
+        {
+
+          categories.map((category) => (
+
+            <GifGrid
+              key={category} 
+              category={category}
+            />
+          ))
+
+        }
+     
     </>
   )
 }
